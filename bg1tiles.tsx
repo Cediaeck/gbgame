32afb52b1e4ec10a2cc685e8911d6ee5faa794bc
Cedric <47922657+Cediaeck@users.noreply.github.com>
2020-08-03 17:19:42 +0200

    tiles

---
+++ bg1tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="bg1tiles" tilewidth="8" tileheight="8" tilecount="72" columns="18">
+ <image source="bg1tiles.png" width="144" height="37"/>
+</tileset>
